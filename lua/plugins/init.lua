db34887933cd3d32ca0f93e2ec7ac08bd7412ae9
Raphael aka theScripter <73601567+Oraclon@users.noreply.github.com>
2025-04-07 11:31:43 +0300

    Create init.lua

---
+++ lua/plugins/init.lua
@@ -0,0 +1,37 @@
+-- ~/.config/nvim/lua/plugins/init.lua
+-- ...
+
+{
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "csharp-language-server",
+      "omnisharp",
+        "xmlformatter",
+        "stylua",
+        "bicep-lsp",
+        "html-lsp",
+        "css-lsp",
+        "csharpier",
+        "prettier",
+        "json-lsp"
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- ensure_installed = "all"
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "c_sharp",
+        "bicep"
+      },
+    },
+  },
